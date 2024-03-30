bf099a034878d5c8e69ca63a82898115525db9dd
NirukshaSandeepa <121809388+NirukshaSandeepa@users.noreply.github.com>
2024-03-30 15:19:39 +0530

    update parser.h

--- parser.h
+++ parser.h
@@ -190,5 +190,378 @@ class parser{
             return t;
         }
 
-    
+        // Read the next token
+        while (i < sizeOfFile || i < 10000 || read[i] != '\0')
+        {
+            // Check if character is a digit
+            if (isDigit(read[i]))
+            {
+                while (isDigit(read[i]))
+                {
+                    num = num + read[i];
+                    i++;
+                }
+                index = i;
+                t.setVal(num);
+                t.setType("INT");
+                return t;
+            }
+            // Check if character is an alphabet letter
+            else if (isAlpha(read[i]))
+            {
+                while (isAlpha(read[i]) || isDigit(read[i]) || read[i] == '_')
+                {
+                    id = id + read[i];
+                    i++;
+                }
+
+                if (isReservedKey(id))
+                {
+                    index = i;
+                    t.setVal(id);
+                    t.setType("KEYWORD");
+                    return t;
+                }
+                else
+                {
+                    index = i;
+                    t.setVal(id);
+                    t.setType("ID");
+                    return t;
+                }
+            }
+            // Check if character is a comment
+            else if (read[i] == '/' && read[i + 1] == '/')
+            {
+                while (read[i] == '\'' || read[i] == '\\' || read[i] == '(' || read[i] == ')' || read[i] == ';' || read[i] == ',' || read[i] == ' ' || read[i] == '\t' || isAlpha(read[i]) || isDigit(read[i]) || isOperator(read[i]))
+                {
+
+                    if (read[i] == '\n')
+                    {
+                        i++;
+                        break;
+                    }
+                    else
+                    {
+                        isComment = isComment + read[i];
+                        i++;
+                    }
+                }
+
+                index = i;
+                t.setVal(isComment);
+                t.setType("DELETE");
+                return t;
+            }
+            // Check if character is an operator
+            else if (isOperator(read[i]))
+            {
+                while (isOperator(read[i]))
+                {
+                    isop = isop + read[i];
+                    i++;
+                }
+
+                index = i;
+                t.setVal(isop);
+                t.setType("OPERATOR");
+                return t;
+            }
+
+            else if (read[i] == '\'')
+            {
+                isString = isString + read[i];
+                i++;
+                while (read[i] == '\'' || read[i] == '\\' || read[i] == '(' || read[i] == ')' || read[i] == ';' || read[i] == ',' || read[i] == ' ' || isAlpha(read[i]) || isDigit(read[i]) || read[i] == '_' || isOperator(read[i]))
+                {
+                    if (read[i] == '\'')
+                    {
+                        isString = isString + read[i];
+                        i++;
+                        break;
+                    }
+                    else if (read[i] == '\\')
+                    {
+                        isString = isString + read[i];
+                        i++;
+                        if (read[i] == 't' || read[i] == 'n' || read[i] == '\\' || read[i] == '"')
+                        {
+                            isString = isString + read[i];
+                            i++;
+                        }
+                        else
+                        {
+                            i++;
+                        }
+                    }
+                    else
+                    {
+                        isString = isString + read[i];
+                        i++;
+                    }
+                }
+                index = i;
+                t.setVal(isString);
+                t.setType("STR");
+                return t;
+            }
+            else if (read[i] == ')' || read[i] == '(' || read[i] == ';' || read[i] == ',')
+            {
+                isPun = isPun + read[i];
+                i++;
+
+                index = i;
+                t.setVal(isPun);
+                t.setType("PUNCTION");
+                return t;
+            }
+            else if (isspace(read[i]))
+            {
+                while (isspace(read[i]))
+                {
+                    isSpace = isSpace + read[i];
+                    i++;
+                }
+                index = i;
+                t.setVal(isSpace);
+                t.setType("DELETE");
+                return t;
+            }
+            else
+            {
+                string temp = "";
+                temp = temp + read[i];
+                t.setVal(temp);
+                t.setType("UNKNOWN");
+                i++;
+                index = i;
+                return t;
+            }
+        }
+
+        return t;
+    }
+
+    // Start parsing
+    void parse()
+    {
+        nextToken = getToken(readnew);          // Get the first token
+        while (nextToken.getType() == "DELETE") // Ignore all DELETE tokens
+            nextToken = getToken(readnew);
+
+        procedure_E(); // Start parsing from E
+
+        while (nextToken.getType() == "DELETE") // Ignore all DELETE tokens
+            nextToken = getToken(readnew);
+
+        if (index >= sizeOfFile - 1) // Check if end of file is reached
+        {
+            tree *t = st.top(); // Get the root of the tree
+
+            // Print the abstact syntax tree if the flag is set
+            if (astFlag == 1)
+                t->print_tree(0);
+
+            // Make the ST
+            makeST(t);
+
+            // Print the standardized tree if the flag is set
+            if (astFlag == 2)
+                t->print_tree(0);
+
+            // Create
+            tree *controlStructureArray[200][200];
+            createControlStructures(t, controlStructureArray);
+
+            int size = 0;
+            int inner = 0;
+            while (controlStructureArray[size][0] != NULL)
+                size++;
+
+            vector<vector<tree *> > setOfControlStruct;
+            for (int i = 0; i < size; i++)
+            {
+                vector<tree *> temp;
+                for (int j = 0; j < 200; j++)
+                {
+                    if (controlStructureArray[i][j] != NULL)
+                        temp.push_back(controlStructureArray[i][j]);
+                }
+                setOfControlStruct.push_back(temp);
+            }
+
+            cse_machine(setOfControlStruct);
+        }
+    }
 }
+
+   // Helper function for makeStandardTree
+    void makeST(tree *t)
+    {
+        makeStandardTree(t);
+    }
+
+    // Makes the tree standard
+    tree *makeStandardTree(tree *t)
+    {
+        if (t == NULL)
+            return NULL;
+        makeStandardTree(t->left);
+        makeStandardTree(t->right);
+
+        if (t->getVal() == "let")
+        {
+            if (t->left->getVal() == "=")
+            {
+                t->setVal("gamma");
+                t->setType("KEYWORD");
+                tree *P = createNode(t->left->right);
+                tree *X = createNode(t->left->left);
+                tree *E = createNode(t->left->left->right);
+                t->left = createNode("lambda", "KEYWORD");
+                t->left->right = E; 
+                tree *lambda = t->left;
+                lambda->left = X;
+                lambda->left->right = P;
+            }
+        }
+        else if (t->getVal() == "and" && t->left->getVal() == "=")
+        {
+            tree *equal = t->left;
+            t->setVal("=");
+            t->setType("KEYWORD");
+            t->left = createNode(",", "PUNCTION");
+            tree *comma = t->left;
+            comma->left = createNode(equal->left);
+            t->left->right = createNode("tau", "KEYWORD");
+            tree *tau = t->left->right;
+
+            tau->left = createNode(equal->left->right);
+            tau = tau->left;
+            comma = comma->left;
+            equal = equal->right;
+
+            while (equal != NULL)
+            {
+                comma->right = createNode(equal->left);
+                comma = comma->right;
+                tau->right = createNode(equal->left->right);
+                tau = tau->right;
+
+                equal = equal->right;
+            }
+        }
+        else if (t->getVal() == "where")
+        {
+            t->setVal("gamma");
+            t->setType("KEYWORD");
+            if (t->left->right->getVal() == "=")
+            {
+                tree *P = createNode(t->left);
+                tree *X = createNode(t->left->right->left);
+                tree *E = createNode(t->left->right->left->right);
+                t->left = createNode("lambda", "KEYWORD");
+                t->left->right = E;
+                t->left->left = X;
+                t->left->left->right = P;
+            }
+        }
+        else if (t->getVal() == "within")
+        {
+            if (t->left->getVal() == "=" && t->left->right->getVal() == "=")
+            {
+                tree *X1 = createNode(t->left->left);
+                tree *E1 = createNode(t->left->left->right);
+                tree *X2 = createNode(t->left->right->left);
+                tree *E2 = createNode(t->left->right->left->right);
+                t->setVal("=");
+                t->setType("KEYWORD");
+                t->left = X2;
+                t->left->right = createNode("gamma", "KEYWORD");
+                tree *temp = t->left->right;
+                temp->left = createNode("lambda", "KEYWORD");
+                temp->left->right = E1;
+                temp = temp->left;
+                temp->left = X1;
+                temp->left->right = E2;
+            }
+        }
+        else if (t->getVal() == "rec" && t->left->getVal() == "=")
+        {
+            tree *X = createNode(t->left->left);
+            tree *E = createNode(t->left->left->right);
+
+            t->setVal("=");
+            t->setType("KEYWORD");
+            t->left = X;
+            t->left->right = createNode("gamma", "KEYWORD");
+            t->left->right->left = createNode("YSTAR", "KEYWORD");
+            tree *ystar = t->left->right->left;
+
+            ystar->right = createNode("lambda", "KEYWORD");
+
+            ystar->right->left = createNode(X);
+            ystar->right->left->right = createNode(E);
+        }
+        else if (t->getVal() == "function_form")
+        {
+            tree *P = createNode(t->left);
+            tree *V = t->left->right;
+
+            t->setVal("=");
+            t->setType("KEYWORD");
+            t->left = P;
+
+            tree *temp = t;
+            while (V->right->right != NULL)
+            {
+                temp->left->right = createNode("lambda", "KEYWORD");
+                temp = temp->left->right;
+                temp->left = createNode(V);
+                V = V->right;
+            }
+
+            temp->left->right = createNode("lambda", "KEYWORD");
+            temp = temp->left->right;
+
+            temp->left = createNode(V);
+            temp->left->right = V->right;
+        }
+        else if (t->getVal() == "lambda")
+        {
+            if (t->left != NULL)
+            {
+                tree *V = t->left;
+                tree *temp = t;
+                if (V->right != NULL && V->right->right != NULL)
+                {
+                    while (V->right->right != NULL)
+                    {
+                        temp->left->right = createNode("lambda", "KEYWORD");
+                        temp = temp->left->right;
+                        temp->left = createNode(V);
+                        V = V->right;
+                    }
+
+                    temp->left->right = createNode("lambda", "KEYWORD");
+                    temp = temp->left->right;
+                    temp->left = createNode(V);
+                    temp->left->right = V->right;
+                }
+            }
+        }
+        else if (t->getVal() == "@")
+        {
+            tree *E1 = createNode(t->left);
+            tree *N = createNode(t->left->right);
+            tree *E2 = createNode(t->left->right->right);
+            t->setVal("gamma");
+            t->setType("KEYWORD");
+            t->left = createNode("gamma", "KEYWORD");
+            t->left->right = E2;
+            t->left->left = N;
+            t->left->left->right = E1;
+        }
+
+        return NULL;
+    }
