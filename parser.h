5cd53ed3847889e40577b9428b2f496c7e2d87d8
NirukshaSandeepa <121809388+NirukshaSandeepa@users.noreply.github.com>
2024-03-30 15:11:04 +0530

    update parser

--- parser.h
+++ parser.h
@@ -27,5 +27,46 @@ string keys[] = {"let", "fn", "in", "where", "aug", "or", "not", "true", "false"
                  "and", "rec", "gr", "ge", "ls", "le", "eq", "ne"};
 
 class parser{
+    public:
+    token nextToken;     // Next token to be read
+    char readnew[10000]; // Stores program for parsing
+    int index;           // Index of character
+    int sizeOfFile;      // Size of file
+    int astFlag;         // Flag to check if AST or ST is to be printed
+
+    parser(char read_array[], int i, int size, int af)
+    {
+        copy(read_array, read_array + 10000, readnew);
+        index = i;
+        sizeOfFile = size;
+        astFlag = af;
+    }
+
+        // Checks if the given string is a keyword
+    bool isReservedKey(string str)
+    {
+        // int size = keys.size();
+        int i = 0;
+        for (i = 0; i < 20; i++)
+        {
+            if (str == keys[i])
+                return true;
+        }
+        return false;
+    }
+
+    // Checks if the given character is an operator
+    bool isOperator(char ch)
+    {
+        for (int i = 0; i < 25; i++)
+        {
+            if (ch == operators[i])
+            {
+                return true;
+            }
+        }
+        return false;
+    }
+
     
 }
